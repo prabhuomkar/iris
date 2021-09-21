e2dcacf0e2047eaa96a5087dac60940edf81e190
Omkar Prabhu <prabhuomkar@pm.me>
2021-09-21 10:15:33 +0530

    Linter fixes

--- api/internal/utils/entity.go
+++ api/internal/utils/entity.go
@@ -9,13 +9,9 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-var (
-	defaultEntityListItemLimit int64
-)
-
 // Returns list of entities based on entity type
 func GetEntitiesByType(ctx context.Context, db *mongo.Connection, entityType string) ([]*models.Entity, error) {
-	defaultEntityListItemLimit = 10
+	defaultEntityListItemLimit := int64(10) // nolint
 
 	cur, err := db.Collection(models.ColEntity).Find(ctx,
 		bson.D{{Key: "entityType", Value: entityType}}, &options.FindOptions{Limit: &defaultEntityListItemLimit})
