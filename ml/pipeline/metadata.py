25a96790c78ec54ef0e318ff86b727cfddface54
Omkar Prabhu <prabhuomkar@pm.me>
2021-09-29 11:09:00 +0530

    Queue manual ack of messages (#30)

--- ml/pipeline/metadata.py
+++ ml/pipeline/metadata.py
@@ -14,22 +14,24 @@ class Metadata(Component):
       tags = exifread.process_file(f)
       if len(tags.keys()) > 0:
         coords = utils.get_gps_coords(tags)
+        media_metadata = {
+          'creationTime': str(tags['EXIF DateTimeOriginal']) if 'EXIF DateTimeOriginal' in tags else None,
+          'width': tags['EXIF ExifImageLength'].values[0] if 'EXIF ExifImageLength' in tags else None,
+          'height': tags['EXIF ExifImageWidth'].values[0] if 'EXIF ExifImageWidth' in tags else None,
+          'location': {
+            'latitude': coords[0] if coords is not None and len(coords) == 2 else None,
+            'longitude': coords[1] if coords is not None and len(coords) == 2 else None,
+          },
+          'photo': {
+            'cameraMake': str(tags['Image Make']) if 'Image Make' in tags else None,
+            'cameraModel': str(tags['Image Model']) if 'Image Model' in tags else None,
+            'focalLength': str(tags['EXIF FocalLength'].values[0].decimal()) if 'EXIF FocalLength' in tags else None,
+            'apertureFNumber': str(tags['EXIF FNumber'].values[0].decimal()) if 'EXIF FNumber' in tags else None,
+            'isoEquivalent': tags['EXIF ISOSpeedRatings'].values[0] if 'EXIF ISOSpeedRatings' in tags else None,
+            'exposureTime': str(tags['EXIF ExposureTime']) if 'EXIF ExposureTime' in tags else None,
+          },
+        }
+        print(f'[metadata]: {media_metadata}')
         self.update({'$set': {
-          'mediaMetadata': {
-            'creationTime': str(tags['EXIF DateTimeOriginal']) if 'EXIF DateTimeOriginal' in tags else None,
-            'width': tags['EXIF ExifImageLength'].values[0] if 'EXIF ExifImageLength' in tags else None,
-            'height': tags['EXIF ExifImageWidth'].values[0] if 'EXIF ExifImageWidth' in tags else None,
-            'location': {
-              'latitude': coords[0] if coords is not None and len(coords) == 2 else None,
-              'longitude': coords[1] if coords is not None and len(coords) == 2 else None,
-            },
-            'photo': {
-              'cameraMake': str(tags['Image Make']) if 'Image Make' in tags else None,
-              'cameraModel': str(tags['Image Model']) if 'Image Model' in tags else None,
-              'focalLength': str(tags['EXIF FocalLength'].values[0].decimal()) if 'EXIF FocalLength' in tags else None,
-              'apertureFNumber': str(tags['EXIF FNumber'].values[0].decimal()) if 'EXIF FNumber' in tags else None,
-              'isoEquivalent': tags['EXIF ISOSpeedRatings'].values[0] if 'EXIF ISOSpeedRatings' in tags else None,
-              'exposureTime': str(tags['EXIF ExposureTime']) if 'EXIF ExposureTime' in tags else None,
-            },
-          }
+          'mediaMetadata': media_metadata
         }})
