ca75d8e78a79ffdaf220220c1394794db455bddf
Omkar Prabhu <prabhuomkar@pm.me>
2021-09-30 09:26:41 +0530

    Updated scripts and commands

--- ml/download_model_archives.sh
+++
@@ -1,2 +0,0 @@
-curl https://torchserve.pytorch.org/mar_files/maskrcnn.mar --output ./models/maskrcnn.mar
-curl https://torchserve.pytorch.org/mar_files/resnet-152-scripted_v2.mar --output ./models/resnet152.mar
