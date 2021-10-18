c43b49323716dc0d450c1bb48f80e3b63d7b18af
Omkar Prabhu <prabhuomkar@pm.me>
2021-10-18 12:00:33 +0530

    Backend fixes for queries and showing entity info per mediaItem (#36)

--- api/internal/models/entity.go
+++ api/internal/models/entity.go
@@ -3,10 +3,11 @@ package models
 const ColEntity = "entities"
 
 type Entity struct {
-	ID         string `json:"id" bson:"_id"`
-	Name       string `json:"name"`
-	ImageURL   string `json:"imageUrl"`
-	EntityType string `json:"entityType"`
-	CreatedAt  string `json:"createdAt"`
-	UpdatedAt  string `json:"updatedAt"`
+	ID         string   `json:"id" bson:"_id"`
+	Name       string   `json:"name"`
+	ImageURL   string   `json:"imageUrl"`
+	EntityType string   `json:"entityType"`
+	MediaItems []string `json:"mediaItems"`
+	CreatedAt  string   `json:"createdAt"`
+	UpdatedAt  string   `json:"updatedAt"`
 }
