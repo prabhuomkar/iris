4fb012a77278a891a2c5ec5f882201f47566ba57
akshaypithadiya <akshay.pithadiya@gmail.com>
2021-12-05 22:54:14 +0530

    Fixed ApolloClient context issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
 import React, { useState, createContext } from 'react';
+import { createUploadLink } from 'apollo-upload-client';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { ThemeProvider } from '@rmwc/theme';
 import { BrowserRouter } from 'react-router-dom';
-import { DrawerAppContent } from '@rmwc/drawer';
 import { Header, Content } from './components';
 import SideNav from './components/SideNav';
+import { DrawerAppContent } from '@rmwc/drawer';
 import '@rmwc/theme/styles';
 import './App.scss';
 export const CreateAlbumContext = createContext();
+
+const link = createUploadLink({ uri: process.env.REACT_APP_API_URL });
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache(),
+});
 
 const App = () => {
   const [open, setOpen] = useState(true);
@@ -15,26 +23,28 @@ const App = () => {
   const [imageList, setImageList] = useState([]);
 
   return (
-    <ThemeProvider
-      options={{
-        primary: '#812ce5',
-        secondary: '#ffffff',
-        onPrimary: '#812ce5',
-        textPrimaryOnBackground: '#000',
-      }}
-    >
-      <CreateAlbumContext.Provider value={{ imageList, setImageList }}>
-        <BrowserRouter>
-          <div className="App">
-            <Header toggleSideNav={toggle} />
-            <SideNav open={open} />
-            <DrawerAppContent>
-              <Content />
-            </DrawerAppContent>
-          </div>
-        </BrowserRouter>
-      </CreateAlbumContext.Provider>
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <CreateAlbumContext.Provider value={{ imageList, setImageList }}>
+          <ThemeProvider
+            options={{
+              primary: '#812ce5',
+              secondary: '#ffffff',
+              onPrimary: '#812ce5',
+              textPrimaryOnBackground: '#000',
+            }}
+          >
+            <BrowserRouter>
+              <Header toggleSideNav={toggle} />
+              <SideNav open={open} />
+              <DrawerAppContent>
+                <Content />
+              </DrawerAppContent>
+            </BrowserRouter>
+          </ThemeProvider>
+        </CreateAlbumContext.Provider>
+      </div>
+    </ApolloProvider>
   );
 };
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,23 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { createUploadLink } from 'apollo-upload-client';
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-import { ApolloProvider } from '@apollo/react-hooks';
 import reportWebVitals from './reportWebVitals';
 
-const link = createUploadLink({ uri: process.env.REACT_APP_API_URL });
-const client = new ApolloClient({
-  link,
-  cache: new InMemoryCache(),
-});
-
-ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
