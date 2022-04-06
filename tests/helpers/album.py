787d7438df8e5436e209128b712e32a8c50de3fa
prabhuomkar <prabhuomkar@pm.me>
2022-04-06 11:17:49 +0530

    More helpers for albums

---
+++ tests/helpers/album.py
@@ -0,0 +1,23 @@
+from .common import get_response
+
+
+def get_albums():
+    pass
+
+def get_album():
+    pass
+
+def create_album():
+    pass
+
+def update_album():
+    pass
+
+def update_album_thumbnail():
+    pass
+
+def update_album_mediaitems():
+    pass
+
+def delete_album():
+    pass
