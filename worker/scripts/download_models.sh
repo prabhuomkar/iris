b6c1eda723a0b50670d00130fd9fb381e5ec2b59
Omkar Prabhu <prabhuomkar@pm.me>
2022-04-17 17:28:09 +0530

    Fixed model download script (#117)

--- worker/scripts/download_models.sh
+++ worker/scripts/download_models.sh
@@ -2,5 +2,5 @@ if [ -f "checkpoints/20180402-114759-vggface2.pt" ]; then
   echo "20180402-114759-vggface2.pt exists"
 else
   echo "downloading 20180402-114759-vggface2.pt"
-  curl https://github.com/timesler/facenet-pytorch/releases/download/v2.2.9/20180402-114759-vggface2.pt --output ./checkpoints/20180402-114759-vggface2.pt
+  curl https://github.com/timesler/facenet-pytorch/releases/download/v2.2.9/20180402-114759-vggface2.pt --location --output ./checkpoints/20180402-114759-vggface2.pt
 fi
